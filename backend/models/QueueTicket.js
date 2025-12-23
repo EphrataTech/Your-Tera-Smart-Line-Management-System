9510401b755c7c675d51b2982097ac08192826ff
RobelAlemayehu <robelalemayehu838@gmail.com>
2025-12-23 12:36:06 +0300

    feat: implement Queue Ticket model aligned with Queue Ticket migration

---
+++ backend/models/QueueTicket.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const { DATE } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const QueueTicket = sequelize.define(
+        'QueueTicket',
+
+        {
+            ticket_id: {
+                type:DataTypes.INTEGER,
+                primaryKey:true,
+                autoIncrement:true,
+                allowNull:false
+            },
+
+            user_id: {
+                type:DataTypes.INTEGER,
+                allowNull:false,
+                references: {
+                    model: 'Users',
+                    key: 'user_id'
+                },
+                onUpdate:"CASCADE",
+                onDelete: 'CASCADE'
+            },
+
+            service_id:{
+                type:DataTypes.INTEGER,
+                allowNull:false,
+                references: {
+                    model: 'Services',
+                    key: 'service_id'
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE'
+            },
+
+            ticket_number: {
+                type:DataTypes.INTEGER,
+                allowNull:false
+            },
+
+            issued_at:{
+                type:DataTypes.DATE,
+                allowNull:false,
+                defaultValue:DataTypes.NOW
+            },
+
+            status: {
+                type:DataTypes.ENUM('Waiting', 'Serving', 'Completed', 'Cancelled'),
+                allowNull:false,
+                defaultValue: 'Waiting'
+            }
+
+        },
+        {
+            tableName: 'Queue_Tickets',
+            timestamps: false,
+            underscored: true
+        }
+
+    );
+
+
+    return QueueTicket
+}
