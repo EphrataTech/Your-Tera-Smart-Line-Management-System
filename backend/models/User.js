3004a6c721caba480f62ac4b029622a3f3f02826
RobelAlemayehu <robelalemayehu838@gmail.com>
2025-12-23 10:58:02 +0300

    feat: implement User model aligned with Users migration

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User',
+        {
+            user_id:{
+                type:DataTypes.INTEGER,
+                primaryKey:true,
+                autoIncrement:true,
+                allowNUll:false
+            },
+            phone_number:{
+                type:DataTypes.STRING,
+                allowNull:false,
+                unique:true
+            },
+            role:{
+                type:DataTypes.ENUM('Customer','Admin'),
+                allowNull:false,
+                defaultValue:'Customer'
+            },
+            created_at:{
+                type:DataTypes.DATE,
+                allowNull:false,
+                defaultValue:DataTypes.NOW
+            }
+        },
+        {
+            tableName:'Users',
+            timestamps:false,
+            underscored:true
+        }
+    );
+    return User;
+}
